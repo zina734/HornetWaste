1b71ff2f7b71a370d604d5622d2dfdefc5f99b2b
Uzair <uzairmacy@gmail.com>
2025-03-09 23:24:01 -0700

    03/09/2025
    - updated UI
      - added start button
      - added level screen
      - enabled click functionality

--- src/gmManager/UI.java
+++ src/gmManager/UI.java
@@ -1,11 +1,9 @@
 package gmManager;
 
 import java.awt.Color;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import javax.swing.JTextArea;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.*;
 
 public class UI {
     gmManager game;             // game executor
@@ -18,32 +16,76 @@ public class UI {
         this.game = game;
 
         createMainField();
-        createBackground();
+        screen();
 
         window.setVisible(true);
     }
 
     public void createMainField() {
         window = new JFrame();
-        window.setSize(1920, 1080);             // set size of window
+        window.setSize(1112, 624);             // set size of window
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().setBackground(Color.white);
         window.setLayout(null);
     }
 
-    public void createBackground() {
-        panel[1] = new JPanel();                // create new JPanel in panel
-        panel[1].setBounds(50, 50, 1920, 1080);       // set bounds for this image
-        panel[1].setLayout(null);
+    public void createBackground(int num, String name) {
+        panel[num] = new JPanel();                // create new JPanel in panel
+        panel[num].setBounds(0, 0, 1920, 1080);       // set bounds for this image
+        panel[num].setLayout(null);
         window.add(panel[1]);                               // adds panel to UI
 
 
-        label[1] = new JLabel();        // create new JLabel in label
-        label[1].setBounds(0, 0, 1920, 1080);               // sets bounds for this image
+        label[num] = new JLabel();        // create new JLabel in label
+        label[num].setBounds(0, 0, 1112, 624);               // sets bounds for this image
 
-        ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("images/honeybarrel1080.jpg"));      // choose image
-        label[1].setIcon(icon);
+        ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource(name));      // choose image
+        label[num].setIcon(icon);
 
-        panel[1].add(label[1]);
+
+    }
+
+    public void createButton(int num, int objx, int objy, int objWidth, int objHeight, String objFile) {
+        JLabel button = new JLabel();                   // object label
+        button.setBounds(objx, objy, objWidth, objHeight);  // object icon
+
+        ImageIcon objIcon = new ImageIcon(getClass().getClassLoader().getResource("images/" + objFile));         // set start button label to "obj"
+        button.setIcon(objIcon);                                                                                      // set start button to the JLabel "button"
+
+        button.addMouseListener(new MouseListener() {
+
+            public void mouseClicked(MouseEvent e) {
+            }
+
+            public void mousePressed(MouseEvent e) {
+                if(SwingUtilities.isLeftMouseButton(e)) {
+                    ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("images/Levelscreen.png"));      // choose image
+                    button.setIcon(null);
+                    button.revalidate();
+                    label[num].setIcon(icon);      // go to level screen
+                }
+            }
+
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            public void mouseExited(MouseEvent e) {
+
+
+            };
+        });
+
+        panel[num].add(button);
+        panel[num].add(label[num]);
+    }
+
+    public void screen() {              // outputs the start screen
+        // Screen 1
+        createBackground(1, "images/background.png");          // set background image
+        createButton(1, 440, 140, 200, 200, "start.png");       // set start button
+
     }
 }
